23d06a5b67fe4399e30aec5469326df00ab3a475
BANVIENCORP\hoang.ngo <hoang.ngo@banvien.com.vn>
2023-12-19 09:52:58 +0700

    Using dbdocs to generate documents for db and gen sql file by dbml file

--- Makefile
+++ Makefile
@@ -1,22 +1,26 @@
 DB_URL=postgresql://postgres:123456@localhost:5432/simple_bank?sslmode=disable
 
 postgres:
-		docker-compose	up	-d
+	docker-compose	up	-d
 createdb:
-		docker	exec	-it	golang-db-1	createdb	--username=postgres	simple_bank
+	docker	exec	-it	golang-db-1	createdb	--username=postgres	simple_bank
 dropdb:
-		docker	exec	-it	golang-db-1	dropdb	simple_bank
+	docker	exec	-it	golang-db-1	dropdb	simple_bank
 migrateup:
 	migrate -path db/migration -database "$(DB_URL)" -verbose up
-
 migrateup1:
 	migrate -path db/migration -database "$(DB_URL)" -verbose up 1
-
 migratedown:
 	migrate -path db/migration -database "$(DB_URL)" -verbose down
-
 migratedown1:
 	migrate -path db/migration -database "$(DB_URL)" -verbose down 1
+new_migration:
+	migrate create -ext sql -dir db/migration -seq $(name)
+db_docs:
+	dbdocs build doc/db.dbml
+
+db_schema:
+	dbml2sql --postgres -o doc/schema.sql doc/db.dbml
 sqlc:
 	sqlc	generate
 test:
@@ -25,4 +29,5 @@ server:
 	go run main.go
 mock:
 	mockgen -package mockdb -destination db/mock/store.go simplebank/db/sqlc Store
-.PHONY:	postgres migrateup migratedown mock
+
+.PHONY:	postgres createdb dropdb migrateup migrateup1 migratedown migratedown1 new_migration db_docs db_schema mock sqlc test server
---
+++ doc/schema.sql
@@ -0,0 +1,68 @@
+-- SQL dump generated using DBML (dbml-lang.org)
+-- Database: PostgreSQL
+-- Generated at: 2023-12-19T02:48:36.869Z
+
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "hashed_password" varchar NOT NULL,
+  "full_name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "accounts" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "owner" varchar NOT NULL,
+  "balance" bigint NOT NULL,
+  "currency" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "entries" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "transfers" (
+  "id" BIGSERIAL PRIMARY KEY,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
+  "amount" bigint NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+CREATE TABLE "sessions" (
+  "id" uuid PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "refresh_token" varchar NOT NULL,
+  "user_agent" varchar NOT NULL,
+  "client_ip" varchar NOT NULL,
+  "is_blocked" bool NOT NULL DEFAULT false,
+  "created_at" timestamptz NOT NULL DEFAULT (now()),
+  "expires_at" timestamptz NOT NULL
+);
+
+CREATE INDEX ON "accounts" ("owner");
+
+CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
+
+CREATE INDEX ON "entries" ("account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+ALTER TABLE "accounts" ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
+
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
+
+ALTER TABLE "sessions" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
